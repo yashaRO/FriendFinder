2b13afcc60426a324ded529a0f96fb0adcc9650b
yashaRO <yashanyou@gmail.com>
2017-02-20 21:19:01 -0600

    Harambified the deities

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,130 @@
 var friendsList = [
 	{
-		"name": "Joshmed",
-		"photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Josh_Sussman_2015.jpg/220px-Josh_Sussman_2015.jpg",
+		"name": "Harambe ALPHA",
+		"photo": "http://static.boredpanda.com/blog/wp-content/uploads/2016/06/gorilla-shot-boy-zookeper-explains-harambe-amanda-odonoughue-cincinnati-zoo-1.jpg",
 		"scores": [
 			3,
-			1,
-			7,
-			3,
+			4,
 			2,
-			1,
+			5,
+			3,
+			5,
 			1,
 			4,
 			2,
-			3
+			2
 		]
 	},
 	{
-  	"name": "Ahmed",
-  	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  	"name": "Harambe BETA",
+  	"photo": "http://pixel.nymag.com/imgs/daily/selectall/2016/07/27/27-harambe-gorilla-heaven.w710.h473.2x.jpg",
   	"scores": [
-    	5,
-    	1,
     	4,
-    	4,
-    	5,
-    	1,
     	2,
+    	3,
     	5,
+    	1,
+    	3,
+    	1,
+    	4,
     	4,
     	1
 		]
 	},
 	{
-  	"name": "Dahmed",
-  	"photo": "http://www.tonedeafcomics.com/wp-content/uploads/2012/11/13191787642011-06-20-Breathe-Dah.gif",
+  	"name": "Harambe DELTA",
+  	"photo": "http://cbsnews2.cbsistatic.com/hub/i/r/2016/06/01/b000ecab-b6dc-441b-a8ce-6d77f3d05070/resize/620x465/abc9a7cd3a9c3a231b51217954125d23/133232115858276749187161410178669586182036o.jpg",
   	"scores": [
-    	2,
     	1,
-    	1,
+    	4,
     	3,
-    	5,
+    	2,
     	3,
     	2,
     	4,
+    	2,
+    	3,
+    	4
+		]
+	},
+	{
+  	"name": "Harambe OMEGA",
+  	"photo": "http://cbsnews1.cbsistatic.com/hub/i/2016/06/01/be84a529-4da6-4981-b3d8-306f852e5001/133086105858279649186878551590185511304691o.jpg",
+  	"scores": [
+    	1,
+    	4,
+    	3,
     	5,
+    	2,
+    	2,
+    	3,
+    	3,
+    	2,
     	1
+		]
+	},
+	{
+  	"name": "Harambe XI",
+  	"photo": "http://www.bfnn.co.uk/wp-content/uploads/2016/06/harambe-750x500.jpg",
+  	"scores": [
+    	5,
+    	4,
+    	2,
+    	1,
+    	2,
+    	2,
+    	1,
+    	5,
+    	2,
+    	3
+		]
+	},
+	{
+  	"name": "Harambe GAMMA",
+  	"photo": "http://www.usnews.com/cmsmedia/4c/c5/7ab7f43445519e1f78303eb29095/160531-harambe-editorial.jpg",
+  	"scores": [
+    	1,
+    	4,
+    	1,
+    	2,
+    	3,
+    	4,
+    	5,
+    	2,
+    	5,
+    	5
+		]
+	},
+	{
+  	"name": "Harambe THETA",
+  	"photo": "http://pixel.nymag.com/imgs/daily/science/2016/09/06/07-harambe-campus-outrage.w710.h473.2x.jpg",
+  	"scores": [
+    	2,
+    	2,
+    	4,
+    	3,
+    	2,
+    	5,
+    	2,
+    	4,
+    	3,
+    	3
+		]
+	},
+	{
+  	"name": "Harambe ZETA",
+  	"photo": "https://i2.wp.com/fusion.net/wp-content/uploads/2016/09/gettyimages-167884840.jpg?resize=1600%2C900&quality=80&strip=all",
+  	"scores": [
+    	2,
+    	3,
+    	5,
+    	3,
+    	5,
+    	5,
+    	4,
+    	1,
+    	1,
+    	2
 		]
 	}
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,7 @@ module.exports = function(app) {
 	
 	app.post('/api/friends', function(req, res) {
 		
-		friendsList.push({
-			'name': req.body.name,
-			'photo': req.body.photo,
-			'scores': [
+		scores = [
 				parseInt(req.body.question1),
 				parseInt(req.body.question2),
 				parseInt(req.body.question3),
@@ -21,9 +18,37 @@ module.exports = function(app) {
 				parseInt(req.body.question8),
 				parseInt(req.body.question9),
 				parseInt(req.body.question10)
-			]
+			];
+		friendsList.push({
+			'name': req.body.name,
+			'photo': req.body.photo,
+			'scores': scores
 		})
 		
-		res.json(friendsList)
+		function friendFinder(curr,list) {
+			
+			tmpArr = []
+			for(let i = 0; i < list.length; i++) {
+				
+				totalDiff = 0
+				
+				for(let j = 0; j < list[i].scores.length; j++) {
+					
+					totalDiff += Math.abs(curr[j] - list[i].scores[j])
+					
+				}
+					tmpArr.push({
+						name:list[i].name,
+						photo:list[i].photo,
+						difference: totalDiff
+					})
+			}							
+			tmpArr.sort((a,b) => a.difference - b.difference)
+			return tmpArr[1];
+		}
+		match = friendFinder(scores,friendsList)
+    //I'd do a whole page and modal, but like, they came on this page for harambe, so they don't need an introduction.
+    
+		res.redirect(match.photo)
 	})
 }
